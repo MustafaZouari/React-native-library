4b9bc55c5c4e7439de6ea87dd416ad1f06da0ee6
zouariM <spoward.dev.front@sossodigit.fr>
2020-11-20 18:15:14 +0100

    start PlayGround

--- src/interfaces/playground/PlayGround.jsx
+++ src/interfaces/playground/PlayGround.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
 
 const PlayGround = () => {
+  const translateX = React.useRef(new Animated.Value(0)).current;
+
   return (
-    <View>
-      <Text>PlayGround</Text>
+    <View style={styles.container}>
+      <TapGestureHandler>
+        <Animated.View style={[styles.box]} />
+      </TapGestureHandler>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    height: 60,
+    width: 60,
+    backgroundColor: "#e63946",
+    borderRadius: 2,
+  },
+});
 export default PlayGround;
